65d7bfaa0ea93b72e1b59b5f19505d65dd6807b7
Andrey Bondarenko <ent101@outlook.com>
2018-07-28 18:53:00 +0300

    add questions

--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -19,32 +19,108 @@ export class StartPageComponent implements OnInit {
 
   question: any;
   questionsArray = [
-    {
-      question: 'вопрос первый?',
-      answer: 'повернуть на лево',
-    },
-    {
-      question: 'вопрос второй?',
-      answer: 'взглянуть вверх',
-    },
-    {
-      question: 'вопрос третий?',
-      answer: 'переступить порог',
-    },
-    {
-      question: 'вопрос четвертый?',
-      answer: 'подняться по ступеньке',
-    },
-    {
-      question: 'вопрос пятый?',
-      answer:`
-подпрыгнуть
-sasas asasa
-  getAnswer(){
-    console.log("qqqq")
+{
+  question: "Какое булево значение вернется если: 1,2,3, Infinity, -Infinity, 'string'",
+  answer: "Ответ: True"
+},
+{
+  question: "Какое булево значение вернется если: 0, -0, null, NaN, Undefined",
+  answer: "Ответ: False"
+},
+{
+  question: `
+Написать инструкцию switch
+let a = 10;
+let b = 20;`,
+  answer: `
+  switch (a) {
+    case 10:
+    console.log('10');
+    break;
+
+    case 20:
+    console.log('20');
+    break;
+
+    default:
+    console.log('default');
+    break;
   }
-`
-    },
+  //10`,
+},
+{
+  question: `
+  Что выведет в консоли:
+
+  let c = 1;
+  for (let i = 0; i < 10; i++) {
+    c = c*2;
+    if (i === 5) {
+      break;
+    }
+    console.log('Шаг ' + i + '- '+ c);
+  }`,
+  answer: `
+  Шаг 0- 2
+  Шаг 1- 4
+  Шаг 2- 8
+  Шаг 3- 16
+  Шаг 4- 32`,
+},
+{
+  question: `
+  Что выведет в консоли:
+
+  let c = 1;
+  for (let i = 0; i < 7; i++) {
+    c = c*2;
+    if (i === 5) {
+      continue;
+    }
+    console.log('Шаг ' + i + '- '+ c);
+  }`,
+  answer:`
+  Шаг 0- 2
+  Шаг 1- 4
+  Шаг 2- 8
+  Шаг 3- 16
+  Шаг 4- 32
+  Шаг 6- 128`
+},
+{
+  question: `
+  Написать инструкцию do while
+  let a = 10;
+  b = 20;
+  c = 0;
+  a = a*2;
+  c < 5;`,
+  answer:`
+  do {
+    a = a*2;
+    console.log(a);
+    c++;
+  }
+  while (c < 5);`
+},
+{
+  question: `
+  Написать инструкцию while
+
+  let a = 2;
+  let i = 0;
+  a = a + 1`,
+  answer:`
+  while (i < 10) {
+    a = a + 1;
+    console.log('Шаг - ' + i + ' Ответ - ' + a);
+    i++;
+  }`
+},
+// {
+//   question: ``,
+//   answer:``
+// },
   ]
 
   getQuestion(id?) {
